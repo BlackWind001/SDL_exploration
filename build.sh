82cc5242c2db16099e2259cdef9765c5ced41085
BlackWind001 <msanirudh1998@gmail.com>
2024-02-18 04:57:04 +0530

    Updated build file to remove the previous executable if it exists.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+if test -f ".temp/main.exe"; then
+  rm .temp/main.exe
+fi
+
 gcc -I "SDL2/x86_64-w64-mingw32/include/" -L "SDL2/x86_64-w64-mingw32/bin/" -L "SDL2/x86_64-w64-mingw32/lib"\
  -o .temp/main.exe *.c -lSDL2 -lSDL2main
 
