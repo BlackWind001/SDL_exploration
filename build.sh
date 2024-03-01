a9750339d6b6c74c73294773a2dada0971e5d331
BlackWind001 <msanirudh1998@gmail.com>
2024-03-01 20:57:12 +0530

    Added SDL_ttf to the linker.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ if test -f ".temp/main.exe"; then
 fi
 
 gcc -I "SDL2/x86_64-w64-mingw32/include/" -L "SDL2/x86_64-w64-mingw32/bin/" -L "SDL2/x86_64-w64-mingw32/lib"\
- -o .temp/main.exe 02-spritesheets/*.c -lSDL2 -lSDL2main
+ -L "SDL2_ttf/x86_64-w64-mingw32/bin/" -L "SDL2_ttf/x86_64-w64-mingw32/lib" -o .temp/main.exe 03-circle/*.c -lSDL2 -lSDL2main
 
 .temp/main.exe
